fec86263904a2a0a90e7a79d0795241f6af4dc97
Jahid Hasan <jahidjjaa@gmail.com>
2021-03-11 02:06:54 +0600

    final addition

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/Home/Home";
 import TeamDetails from "./components/TeamDetails/TeamDetails";
 import TeamList from "./components/TeamList/TeamList";
 import "bootstrap/dist/css/bootstrap.min.css";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
@@ -21,6 +22,9 @@ function App() {
           </Route>
           <Route path="/teamDetails/:id">
             <TeamDetails></TeamDetails>
+          </Route>
+          <Route path='*'>
+            <NotFound></NotFound>
           </Route>
         </Switch>
       </Router>
